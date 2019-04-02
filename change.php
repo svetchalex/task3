d43e8e9ea758f7bcead2d2ef5d9acd81459b4a27
Sveta Chernokutsatova <svetchalex@gmail.com>
2019-04-02 12:10:37 +0300

    Задачи по БД

--- change.php
+++ change.php
@@ -2,10 +2,28 @@
 function changeTable()
 {
     $mysqli = new mysqli('localhost', 'stud03', 'password', 'database');
-    $a = rand(1,3); $b=rand(1,140000);
+    if (!mysqli_set_charset($mysqli, "utf8")) {
+    printf("Ошибка при загрузке набора символов utf8: %s\n", mysqli_error($mysqli));
+    exit();
+} else {
+    printf("Текущий набор символов: %s\n", mysqli_character_set_name($mysqli));
+}
+
+
+
+    $a = rand(1,1000)
     $sql1 = <<<SQL
-       INSERT INTO contactgroup (id, id_group, id_contact) VALUES
-       (null, $a, $b)
+       INSERT INTO contacts (id, name, telephone, id_client) VALUES
+       (null, 'Яхлаков Лукьян Валериевич', '74953478043', $a ),
+       (null, 'Нырцева Майя Виталиевна', '74956107808', $a ),
+       (null, 'Журбин Парфен Остапович', '74957968289', $a ),
+       (null, 'Михайличенко Людмила Ефимовна', '74955368858', $a ),
+       (null, 'Викаш Аркадий Аникитевич', '74959600040', $a ),
+       (null, 'Гавриленкова Александра Елизаровна', '74955054136', $a ),
+       (null, 'Сиянских Владимир Сигизмундович', '74952375171', $a ),
+       (null, 'Кирилов Артём Филимонович ', '74950859331', $a ),
+       (null, 'Бобков Николай Михеевич', '74959220637', $a ),
+       (null, 'Пирожков Михаил Дмитриевич ', '74954266625', $a )
        
 SQL;
     try {
