005243c49e979f1455117f52933f549f9f2d570c
Sveta Chernokutsatova <svetchalex@gmail.com>
2019-04-02 12:39:29 +0300

    Задачи по БД

---
+++ changegroup.php
@@ -0,0 +1,37 @@
+<?php
+function changeTable($a)
+{
+    $mysqli = new mysqli('localhost', 'stud03', 'password', 'contacts');
+
+
+
+
+
+    $sql1 = <<<SQL
+       INSERT INTO contacts (id, name, telephone, id_client) VALUES
+       (null, 'Часто используемые',  $a ),
+       (null, 'Семья', $a ),
+       (null, 'Знакомые', $a ),
+       (null, 'Друзья',  $a ),
+       (null, 'Сотрудники',$a ),
+      
+       
+SQL;
+    try {
+
+        if (!$res = $mysqli->query($sql1)) {
+            throw new Exception($mysqli->error);
+        }
+    } catch (Exception $e) {
+        echo 'Error: ', $e->getMessage(), "\n";
+    }
+
+    return true;
+}
+
+function change(){
+    for($i=1; $i<1001;$i++){
+        changeTable($i);
+    }
+}
+change();
